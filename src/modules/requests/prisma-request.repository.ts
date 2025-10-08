6dd543c5e8aa07b7dfeb2b54f896c672f4c78577
Lee Yujin <yuwls1102@gmail.com>
2025-10-09 02:35:09 +0900

    refactor: 기존에 만들어진 커스텀 에러를 사용하도록 수정 #33

--- src/modules/requests/prisma-request.repository.ts
+++ src/modules/requests/prisma-request.repository.ts
@@ -2,7 +2,7 @@ import { Prisma, Request } from '@prisma/client';
 import { IRequestRepository } from './interface/request.repository.interface';
 import { PrismaService } from '@/shared/prisma/prisma.service';
 import { Injectable } from '@nestjs/common';
-import { PendingRequestConflictError } from './interface/errors';
+import { ConflictException } from '@/shared/exceptions';
 import { CreateRequestData, RequestEntity } from './types';
 
 @Injectable()
@@ -41,7 +41,9 @@ export class PrismaRequestRepository implements IRequestRepository {
     } catch (e) {
       if (e instanceof Prisma.PrismaClientKnownRequestError && e.code === 'P2002') {
         // 옵션: e.meta?.target으로 특정 인덱스 식별 가능
-        throw new PendingRequestConflictError();
+        throw new ConflictException(
+          '이미 진행중인 요청이 있습니다. 기존 요청을 완료하거나 취소한 뒤 다시 시도해주세요.',
+        );
       }
       throw e;
     }
