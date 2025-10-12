4c0c7a8a13182a7e541e7d77e6558e652384c9a1
dudska12 <skadud0113@naver.com>
2025-10-12 22:54:50 +0900

    feat:받은요청GET api 구현 #39

--- src/modules/requests/interface/request.service.interface.ts
+++ src/modules/requests/interface/request.service.interface.ts
@@ -1,9 +1,10 @@
 import { AccessTokenPayload } from '@/shared/jwt/jwt.payload.schema';
 import { CreateQuoteRequestBody } from '../dto/create-quote-request.dto';
 import { Request } from '@prisma/client';
-
+import { ReceivedRequest } from '../dto/request-quote-request-received.dto';
 export interface IRequestService {
   createQuoteRequest(createQuoteRequestBody: CreateQuoteRequestBody, user: AccessTokenPayload): Promise<Request>;
+  findReceivedByDriverId(driverId: string): Promise<ReceivedRequest[]>;
 }
 
 export const REQUEST_SERVICE = 'IRequestService';
