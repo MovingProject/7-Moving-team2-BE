47fa9c806fe9b346d2e7b52bb7fd6e2df209fd3a
dudska12 <skadud0113@naver.com>
2025-10-12 22:26:51 +0900

    feat: 받은요청 dto 설계 #39

---
+++ src/modules/requests/dto/request-quote-request-received.dto.ts
@@ -0,0 +1,25 @@
+import { z } from 'zod';
+import { MoveType } from '@prisma/client';
+
+/**
+ * @description 받은 요청 단일 아이템
+ */
+export const ReceivedRequestSchema = z.object({
+  id: z.string().uuid(),
+  consumerName: z.string(),
+  moveAt: z.coerce.date(),
+  departureAddress: z.string(),
+  arrivalAddress: z.string(),
+  serviceType: z.nativeEnum(MoveType),
+  createdAt: z.coerce.date(),
+});
+
+/**
+ * @description 받은 요청 목록 응답
+ */
+export const ReceivedRequestsResponseSchema = z.array(ReceivedRequestSchema);
+
+
+// 타입추론
+export type ReceivedRequest = z.infer<typeof ReceivedRequestSchema>;
+export type ReceivedRequestsResponse = z.infer<typeof ReceivedRequestsResponseSchema>;
