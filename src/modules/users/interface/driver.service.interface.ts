d3bdb3eaf0ebb1ddb51396fb6cb14986b0033cf6
Lee Yujin <yuwls1102@gmail.com>
2025-10-12 20:41:50 +0900

    feat: 드라이버 좋아요 취소 기능 구현 #41

--- src/modules/users/interface/driver.service.interface.ts
+++ src/modules/users/interface/driver.service.interface.ts
@@ -1,9 +1,11 @@
 import { AccessTokenPayload } from '@/shared/jwt/jwt.payload.schema';
 
 type LikeDriverResult = { liked: true } | { liked: false; message: 'Already liked' };
+type UnlikeDriverResult = { unliked: true } | { unliked: false; message: 'Already unliked' };
 
 export interface IDriverService {
   likeDriver(driverId: string, user: AccessTokenPayload): Promise<LikeDriverResult>;
+  unlikeDriver(driverId: string, user: AccessTokenPayload): Promise<UnlikeDriverResult>;
 }
 
 export const DRIVER_SERVICE = 'IDriverService';
--- src/modules/users/interface/like.repository.interface.ts
+++ src/modules/users/interface/like.repository.interface.ts
@@ -2,6 +2,7 @@ import { TransactionContext } from '@/shared/prisma/transaction-runner.interface
 
 export interface ILikeRepository {
   insertIfAbsent(consumerId: string, driverId: string, ctx?: TransactionContext): Promise<boolean>;
+  deleteIfExists(consumerId: string, driverId: string, ctx?: TransactionContext): Promise<boolean>;
 }
 
 export const LIKE_REPOSITORY = 'ILikeRepository';
