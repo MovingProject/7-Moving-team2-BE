b3b9ef3ed7f79659ac8da145725c50f7f5c95dae
dudska12 <skadud0113@naver.com>
2025-10-14 16:30:23 +0900

    feat:post방식 filter api 구현 #47

--- src/modules/requests/interface/request.repository.interface.ts
+++ src/modules/requests/interface/request.repository.interface.ts
@@ -2,11 +2,13 @@ import { CreateRequestData, RequestEntity } from '../types';
 import { ReceivedRequest } from '../dto/request-quote-request-received.dto';
 import { TransactionContext } from '@/shared/prisma/transaction-runner.interface';
 
+import { ReceivedRequestFilter } from '../dto/request-filter-post.dto';
 export interface IRequestRepository {
   findPendingByConsumerId(consumerId: string, ctx?: TransactionContext): Promise<RequestEntity | null>;
   createRequest(data: CreateRequestData): Promise<RequestEntity>;
   findInvitesByDriverId(driverId: string): Promise<ReceivedRequest[]>;
   incrementInvitedCountIfAvailable(requestId: string, ctx?: TransactionContext): Promise<boolean>;
+  filterRequests(driverId: string, filter: ReceivedRequestFilter): Promise<ReceivedRequest[]>;
 }
 
 export const REQUEST_REPOSITORY = 'IRequestRepository';
