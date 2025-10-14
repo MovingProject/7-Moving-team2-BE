44f0fee45c6dbd46406c9cb7fa71a65d0f02946f
dudska12 <skadud0113@naver.com>
2025-10-14 13:03:58 +0900

    refactor: Received dto isInvited 추가 #50

--- src/modules/requests/dto/request-quote-request-received.dto.ts
+++ src/modules/requests/dto/request-quote-request-received.dto.ts
@@ -12,13 +12,13 @@ export const ReceivedRequestSchema = z.object({
   arrivalAddress: z.string(),
   serviceType: z.nativeEnum(MoveType),
   createdAt: z.coerce.date(),
+  isInvited: z.boolean(),
 });
 
 /**
  * @description 받은 요청 목록 응답
  */
 export const ReceivedRequestsResponseSchema = z.array(ReceivedRequestSchema);
-
 
 // 타입추론
 export type ReceivedRequest = z.infer<typeof ReceivedRequestSchema>;
