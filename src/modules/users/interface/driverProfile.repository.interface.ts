8aa6f309051aebc8b90cdf75e0290658e44f85b4
Lee Yujin <yuwls1102@gmail.com>
2025-10-14 17:57:50 +0900

    feat: Driver 프로필 등록 API 구현 #6

--- src/modules/users/interface/driverProfile.repository.interface.ts
+++ src/modules/users/interface/driverProfile.repository.interface.ts
@@ -1,6 +1,13 @@
 import { TransactionContext } from '@/shared/prisma/transaction-runner.interface';
+import { CreateDriverProfileBody } from '../dto/createDriverProfileBodySchema';
+import { DriverProfileEntity } from '../types';
 
 export interface IDriverProfileRepository {
+  createDriverProfile(
+    driverId: string,
+    body: CreateDriverProfileBody,
+    ctx?: TransactionContext,
+  ): Promise<DriverProfileEntity>;
   incrementLikeCount(driverId: string, ctx?: TransactionContext): Promise<void>;
   decrementLikeCount(driverId: string, ctx?: TransactionContext): Promise<void>;
 }
