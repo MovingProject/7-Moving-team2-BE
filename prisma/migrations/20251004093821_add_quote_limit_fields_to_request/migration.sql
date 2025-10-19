2e9f0ecbb727cad10fb668127c747209241fea57
Lee Yujin <yuwls1102@gmail.com>
2025-10-20 00:06:55 +0900

    chore(migrations): remove old migrations to rebuild baseline

--- prisma/migrations/20251004093821_add_quote_limit_fields_to_request/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "public"."Request" ADD COLUMN     "generalQuoteCount" INTEGER NOT NULL DEFAULT 0,
-ADD COLUMN     "generalQuoteLimit" INTEGER NOT NULL DEFAULT 5,
-ADD COLUMN     "invitedQuoteCount" INTEGER NOT NULL DEFAULT 0,
-ADD COLUMN     "invitedQuoteLimit" INTEGER NOT NULL DEFAULT 3;
