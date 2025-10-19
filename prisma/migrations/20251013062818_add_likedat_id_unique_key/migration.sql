dd049f0f7be889d87b65cedc2c466f07f85d61cf
Lee Yujin <yuwls1102@gmail.com>
2025-10-20 00:06:55 +0900

    chore(migrations): remove old migrations to rebuild baseline

--- prisma/migrations/20251013062818_add_likedat_id_unique_key/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[likedAt,id]` on the table `Like` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Like_likedAt_id_key" ON "public"."Like"("likedAt", "id");
