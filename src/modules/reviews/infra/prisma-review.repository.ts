0dcda69cca695d3813c92395bb713cd406c502e4
dudska12 <skadud0113@naver.com>
2025-10-21 10:18:10 +0900

    feat:리뷰요청api 구현 #69

--- src/modules/reviews/infra/prisma-review.repository.ts
+++ src/modules/reviews/infra/prisma-review.repository.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { IReviewRepository } from '../interface/review-repository.interface';
 import { PrismaService } from '@/shared/prisma/prisma.service';
+import { reviewInput } from '../dto/review.create.dto';
+import { Quotation, Review } from '@prisma/client';
 
 @Injectable()
 export class PrismaReviewRepository implements IReviewRepository {
   constructor(private readonly prisma: PrismaService) {}
+
+  async createReview(input: reviewInput): Promise<Review> {
+    return this.prisma.review.create({ data: input });
+  }
+
+  async findByQuotationId(quotationId: string) {
+    return this.prisma.review.findUnique({ where: { quotationId } });
+  }
+
+  async findQuotationById(quotationId: string): Promise<Quotation | null> {
+    return this.prisma.quotation.findUnique({
+      where: { id: quotationId },
+    });
+  }
 }
