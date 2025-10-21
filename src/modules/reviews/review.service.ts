280a47f3a0def4bb0f5c23c45789243acc2c44d5
dudska12 <skadud0113@naver.com>
2025-10-21 11:03:25 +0900

    feat:리뷰요청 에러핸들링 #69

--- src/modules/reviews/review.service.ts
+++ src/modules/reviews/review.service.ts
@@ -4,7 +4,7 @@ import { REVIEW_REPOSITORY } from './interface/review-repository.interface';
 import { readonly } from 'zod/v4';
 import type { IReviewRepository } from './interface/review-repository.interface';
 import { reviewDTO, reviewInput } from './dto/review.create.dto';
-import { BadRequestException } from '@/shared/exceptions';
+import { BadRequestException, ForbiddenException } from '@/shared/exceptions';
 
 @Injectable()
 export class ReviewService implements IReviewService {
@@ -19,6 +19,15 @@ export class ReviewService implements IReviewService {
 
     const quotation = await this.reviewRepository.findQuotationById(input.quotationId);
     if (!quotation) throw new BadRequestException('존재하지 않는 견적입니다.');
+
+    if (quotation.consumerId !== input.consumerId) {
+      throw new ForbiddenException('본인의 견적에만 리뷰를 작성할 수 있습니다.');
+    }
+
+    if (quotation.status !== 'SELECTED') {
+      throw new BadRequestException('이사가 완료된 견적만 리뷰를 작성할 수 있습니다.');
+    }
+    //자꾸 까먹게되네 이게맞나?
 
     const reviewData: reviewInput = { ...input, driverId: quotation.driverId };
 
