fdc066fe7b4b5ff8eeeee3f5218a4478b8234bed
dudska12 <skadud0113@naver.com>
2025-10-24 16:43:54 +0900

    feat:리뷰평점 가져오기 api 구현 #72

--- src/modules/reviews/interface/review-repository.interface.ts
+++ src/modules/reviews/interface/review-repository.interface.ts
@@ -8,6 +8,7 @@ export interface IReviewRepository {
   findByQuotationId(quotationId: string): Promise<Review | null>;
   findQuotationById(quotationId: string): Promise<Quotation | null>;
   findReviewsByDriverId(driverId: string, limit: number, cursor?: string): Promise<ReviewWithConsumer[]>;
+  findRatingStatsByDriverId(driverId: string): Promise<{ rating: number; _count: { rating: number } }[]>;
 }
 
 export const REVIEW_REPOSITORY = 'IReviewRepository';
