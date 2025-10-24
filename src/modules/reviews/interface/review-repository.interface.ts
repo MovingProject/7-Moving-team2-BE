da4a7316abc450633997b41722b3ff3e296b324b
dudska12 <skadud0113@naver.com>
2025-10-24 16:18:43 +0900

    feat:리뷰댓글 가져오기 api 구현 #72

--- src/modules/reviews/interface/review-repository.interface.ts
+++ src/modules/reviews/interface/review-repository.interface.ts
@@ -1,10 +1,13 @@
 import { Quotation, Review } from '@prisma/client';
 import { reviewInput } from '../dto/review.create.dto';
 
+export type ReviewWithConsumer = Review & { consumer: { name: string } };
+
 export interface IReviewRepository {
   createReview(input: reviewInput): Promise<Review>;
   findByQuotationId(quotationId: string): Promise<Review | null>;
   findQuotationById(quotationId: string): Promise<Quotation | null>;
+  findReviewsByDriverId(driverId: string, limit: number, cursor?: string): Promise<ReviewWithConsumer[]>;
 }
 
 export const REVIEW_REPOSITORY = 'IReviewRepository';
