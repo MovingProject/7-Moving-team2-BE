3ba0b1f15495565f369d68ddc159da7d0ce69145
dudska12 <skadud0113@naver.com>
2025-10-27 09:48:23 +0900

    fix:dto 수정및 Quotation 분리 및 에러순서 변경 #69

---
+++ src/modules/reviews/interface/quotation-repository.interface.ts
@@ -0,0 +1,8 @@
+import { Quotation } from "@prisma/client";
+
+
+export interface IQuotationRepository {
+  findQuotationById(quotationId: string): Promise<Quotation | null>;
+}
+
+export const QUOTATION_REPOSITORY = 'IQuotationRepository';
