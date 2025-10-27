0fa5fd05ad0e162dc1788a6640304213a4650ad1
dudska12 <skadud0113@naver.com>
2025-10-27 10:27:02 +0900

    fix: reviewDTO -> reviewDto 컨벤션규칙에따라 변경

--- src/modules/reviews/interface/review-service.interface.ts
+++ src/modules/reviews/interface/review-service.interface.ts
@@ -1,9 +1,9 @@
 import { Review } from '@prisma/client';
-import { reviewDTO, reviewInput } from '../dto/review.create.dto';
 import { ReviewListResponseDto } from '../dto/review.get.dto';
+import { reviewDto, reviewInput } from '../dto/review.create.dto';
 
 export interface IReviewService {
-  createReview(input: reviewDTO & { consumerId: string }): Promise<Review>;
+  createReview(input: reviewDto & { consumerId: string }): Promise<Review>;
   getDriverReviews(driverId: string, limit: number, cursor?: string): Promise<ReviewListResponseDto>;
   getDriverRatingDistribution(driverId: string): Promise<{
     driverId: string;
@@ -11,6 +11,7 @@ export interface IReviewService {
     averageRating: number;
     ratings: Record<number, number>;
   }>;
+
 }
 
 export const REVIEW_SERVICE = 'IReviewService';
