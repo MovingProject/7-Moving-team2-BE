225427285865b807a9f68f0472ec44e85990507b
dudska12 <skadud0113@naver.com>
2025-10-28 18:14:30 +0900

    feat:module연결 및 테스트 #90

--- src/modules/quotations/quotation.module.ts
+++ src/modules/quotations/quotation.module.ts
@@ -4,9 +4,12 @@ import { PrismaService } from '@/shared/prisma/prisma.service';
 import { QuotationService } from './quotation.service';
 import { QUOTATION_REPOSITORY } from './interface/quotation.repository.interface';
 import { PrismaQuotationRepository } from './infra/prisma-quotation.repository';
+import { UsersModule } from '../users/users.module';
+import { PrismaModule } from '@/shared/prisma/prisma.module';
+import { CookieModule } from '@/shared/utils/cookie.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, PrismaModule, CookieModule],
   controllers: [QuotationController],
   providers: [
     PrismaService,
