7920d0cd53167856bb1f96dce8ce8814c93ddc64
KimJesung <andrewkim99@naver.com>
2025-10-29 18:15:44 +0900

    refactor: requestId 외의 다른 데이터도 가져오게 수정 #85

--- src/modules/requests/dto/request-check.dto.ts
+++ src/modules/requests/dto/request-check.dto.ts
@@ -3,6 +3,17 @@ export const RequestCheckSchema = z.object({
   pendingRequest: z
     .object({
       id: z.string(),
+      serviceType: z.string(),
+      moveAt: z.string(),
+      departureAddress: z.string(),
+      departureFloor: z.number(),
+      departureElevator: z.boolean(),
+      departurePyeong: z.number(),
+      arrivalAddress: z.string(),
+      arrivalFloor: z.number(),
+      arrivalElevator: z.boolean(),
+      arrivalPyeong: z.number(),
+      additionalRequirements: z.string().nullable().optional(),
     })
     .nullable(),
 });
--- src/modules/requests/infra/prisma-request.repository.ts
+++ src/modules/requests/infra/prisma-request.repository.ts
@@ -255,5 +255,4 @@ export class PrismaRequestRepository implements IRequestRepository {
       orderBy: { createdAt: 'desc' },
     });
   }
-
 }
