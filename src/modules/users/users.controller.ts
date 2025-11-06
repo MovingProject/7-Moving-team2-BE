5b280735671838a4fcb7854c17e5d47b88e58e0b
Lee Yujin <yuwls1102@gmail.com>
2025-11-06 16:34:49 +0900

    refactor(users): 드라이버소비자 프로필을 단일 DTO로 매핑하여 /me 응답 개선 #142

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,34 +1,23 @@
 import type { AccessTokenPayload } from '@/shared/jwt/jwt.payload.schema';
-import { Body, Controller, Get, NotFoundException, Patch, Req, Res, UseGuards } from '@nestjs/common';
-import { ApiNotFoundResponse, ApiOkResponse, ApiOperation } from '@nestjs/swagger';
+import { Body, Controller, Get, Patch, Req, UseGuards } from '@nestjs/common';
+import { ApiNotFoundResponse, ApiOperation } from '@nestjs/swagger';
 import { AuthUser } from '../auth/decorators/auth-user.decorator';
 import { AccessTokenGuard } from '../auth/guards/accessToken.guard';
-import { EditConsumerProfileDto, UserDtoFactory } from './dto/user.response.dto';
 import { UpdateUserProfileDto } from './dto/user.update.dto';
 import type { AuthenticatedRequest } from './interface/users.repository.interface';
 import { UsersService } from './users.service';
+
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
   @Get('me')
   @UseGuards(AccessTokenGuard)
   @ApiOperation({ summary: '유저 프로필 수정시 GET 데이터(유저정보)' })
-  @ApiOkResponse({ type: EditConsumerProfileDto })
   @ApiNotFoundResponse({ description: '프로필이 없습니다.' })
-  async getProfile(@AuthUser() authUser: AccessTokenPayload, @Res({ passthrough: true }) _res: Response) {
-    const user = await this.usersService.getUserWithProfile(authUser.sub);
-    if (user.consumerProfile) {
-      return UserDtoFactory.toEditConsumerProfileDto(user, user.consumerProfile);
-    } else if (user.driverProfile) {
-      return UserDtoFactory.toEditDriverProfileDto(
-        user,
-        user.driverProfile,
-        user.driverProfile.driverServiceTypes?.map((t) => t.serviceType),
-        user.driverProfile.driverServiceAreas?.map((a) => a.serviceArea),
-      );
-    } else {
-      throw new NotFoundException('프로필이 없습니다.');
-    }
+  async getProfile(@AuthUser() authUser: AccessTokenPayload) {
+    const user = await this.usersService.getUserwithFullProfile(authUser.sub);
+    return user;
   }
 
   @Patch('me/profile')
