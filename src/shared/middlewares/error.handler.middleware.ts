69c7b4c6d4b7e8d003c45fdc7fdd95dff29bb565
Lee Yujin <yuwls1102@gmail.com>
2025-11-07 10:23:25 +0900

    fix(oauth): 콜백에서 중복 응답 발생 문제 수정 #142

--- src/shared/middlewares/error.handler.middleware.ts
+++ src/shared/middlewares/error.handler.middleware.ts
@@ -2,6 +2,11 @@ import { NextFunction, Request, Response } from 'express';
 import { HttpException } from '../exceptions/http.exception';
 
 export const errorHandler = (err: Error, req: Request, res: Response, _next?: NextFunction) => {
+  if (res.headersSent) {
+    console.error('Headers already sent, skipping errorHandler', err);
+    return;
+  }
+
   if (err instanceof HttpException) {
     return res.status(err.status).json({
       success: false,
